59c3e233fa706aec8082e567e35455557a329e12
HP <benesi-fundos>
2021-08-27 11:46:24 -0700

    added

---
+++ app/src/main/java/com/example/medicalsupportsystem/Model.java
@@ -0,0 +1,41 @@
+package com.example.medicalsupportsystem;
+
+public class Model {
+    String fullname, idnum, distict, ta, village, psign1, psign2, psign3, psign4;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getIdnum() {
+        return idnum;
+    }
+
+    public String getDistict() {
+        return distict;
+    }
+
+    public String getTa() {
+        return ta;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public String getPsign1() {
+        return psign1;
+    }
+
+    public String getPsign2() {
+        return psign2;
+    }
+
+    public String getPsign3() {
+        return psign3;
+    }
+
+    public String getPsign4() {
+        return psign4;
+    }
+}
